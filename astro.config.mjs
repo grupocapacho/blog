3f61f2cd6fe209328241822452a6686047cbcde4
Juan Pablo Pérez <juan.perez@lorspi.com>
2025-08-28 10:44:10 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://blog.lorspi.com/',
-<<<<<<< HEAD
     base: '/',
-=======
-    base: '/blog/',
->>>>>>> d14384583de2989c60f43290978dd723ad175a9e
     vite: {
         plugins: [tailwindcss()]
     },
